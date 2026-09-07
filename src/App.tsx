2919249d589c3231d3f766e40a0f0f44aa261948
Katelyn Luu <kcluu16@gmail.com>
2026-09-07 19:05:00 +0000

    chore: scaffold Vite + React + TypeScript app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="page">
+      <h1>Fan Score</h1>
+      <p>Coming soon.</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+
+import App from './App'
+import './index.css'
+
+const rootElement = document.getElementById('root') as HTMLElement
+
+createRoot(rootElement).render(<App />)
